6b7d483d1ad67275b71f5eec7036ee78d8248c81
fceek <fceek96@gmail.com>
2018-11-22 20:39:01 +0000

    Binary search in a ordered list

---
+++ Search/binarySearch.js
@@ -0,0 +1,13 @@
+var target = 12;
+var list = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+function binarySearch(lb,rb) {
+
+    let mpt = Math.floor((lb + rb)/2);
+
+    if (target == list[mpt]) return mpt;
+    if (target <  list[mpt]) return binarySearch(lb,mpt-1);
+    if (target >  list[mpt]) return binarySearch(mpt+1,rb);
+}
+
+console.log(binarySearch(0,list.length-1));
