d0f534ac859169a880d19f90aeac9804c1f57256
fceek <fceek96@gmail.com>
2018-11-26 16:53:07 +0000

    edited to fit class in 26 Nov. Monday

--- inClass/23Nov.js
+++ inClass/23Nov.js
@@ -33,27 +33,20 @@ function productOfIndexes() {
 
 function getAllFactors(number) {  // This function is to get the input's every factor(including 1 and itself)
 
+    let count = 0;
 
     for (let i = 1; i <= number; i++) {
-        if (number % i == 0) console.log(i + ' ');
-    }
-}
-
-function checkPrime(number) {  // This function is to check if the input is a prime number
-
-    let flag = true;
-    let upper = Math.floor(Math.sqrt(number)) + 1; //Upper boundary set to square root of input, because factors are always appeared in pairs both sides of square root of input, we don't need to check both sides
-
-    for (let i = 2; i < upper; i++) {
-        if (number % i == 0) flag = false;
+        if (number % i == 0) {
+            console.log(i + ' ');
+            count++;
+        }
     }
 
-    if (flag) console.log('is prime');
-        else console.log('not prime')
+    console.log('Total: ' + count);
 }
+
 
 addIndexes();
 combinationOfIndexes();
 productOfIndexes();
-getAllFactors(21371);
-checkPrime(21371);
+getAllFactors(65536);
---
+++ inClass/26Nov.js
@@ -0,0 +1,20 @@
+function checkPrime(number) {  // This function is to check if the input is a prime number
+
+    let flag = true;
+    let upper = Math.floor(Math.sqrt(number)) + 1; //Upper boundary set to square root of input, because factors are always appeared in pairs both sides of square root of input, we don't need to check both sides
+
+    for (let i = 2; i < upper; i++) {
+        if (number % i == 0) flag = false;
+    }
+
+    if (flag) console.log('is prime');
+    else console.log('not prime')
+}
+
+function goesToOne(number) {
+    //nothing here
+}
+
+
+checkPrime(21371);
+goesToOne(21371);
