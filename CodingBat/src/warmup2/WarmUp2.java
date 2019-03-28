9e7cb88138760f6a69789f3fb1a3aebd5adf1edc
fceek <fceek96@gmail.com>
2019-03-28 23:40:32 +0000

    Coding Bat Warmup 2

--- CodingBat/src/warmup2/WarmUp2.java
+++ CodingBat/src/warmup2/WarmUp2.java
@@ -6,7 +6,7 @@ public class WarmUp2 {
         Code runner = new Code() ;
 
         System.out.println(
-                runner.arrayCount9(new int[]{1, 9, 9,3,9})
+                runner.noTriples(new int[]{1,1,2,2,1})
         );
     }
 
@@ -93,8 +93,87 @@ class Code {
     }
 
     public boolean arrayFront9(int[] nums) {
+        boolean found9 = false;
+        if (nums.length <= 4) {
+            for (int index: nums) {
+                if (index == 9) found9 = true;
+            }
+        }
+        else {
+            for (int i = 0; i < 4; i++) {
+                if (nums[i] == 9) found9 =true;
+            }
+        }
 
+        return found9;
     }
 
+    public boolean array123(int[] nums) {
+        if (nums.length < 3) return false;
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (nums[i] == 1 && nums[i+1] == 2 && nums[i+2] == 3) return true;
+        }
+        return false;
+    }
 
+    public int stringMatch(String a, String b) {
+        if (a.length() < 2|| b.length() < 2) return 0;
+        int matchCount = 0;
+        int shorter = a.length();
+        if (a.length() > b.length())  shorter = b.length();
+        for (int i = 0; i <= shorter - 2; i++) {
+            String sample = a.substring(i,i+2);
+            if (sample.equals(b.substring(i,i+2))) matchCount++;
+        }
+
+        return matchCount;
+    }
+
+    public String stringX(String str) {
+        if (str.length() < 3) return str;
+        String procStr = "";
+        for (int i = 1; i <= str.length() - 2; i++) {
+            if (str.charAt(i) != 'x') procStr += str.charAt(i);
+        }
+        procStr = str.charAt(0) + procStr + str.charAt(str.length() - 1);
+        return procStr;
+    }
+
+    public String altPairs(String str) {
+        String procStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i % 4 < 2) procStr += str.charAt(i);
+        }
+        return procStr;
+    }
+
+    public String stringYak(String str) {
+        int len = str.length();
+        if (len < 3) return str;
+        String procStr = "";
+        int flag = 0;
+        for (int i = 1; i < len - 1 ; i++) {
+            if (str.charAt(i-1) == 'y' && str.charAt(i+1) == 'k') {
+                procStr += str.substring(flag,i-1);
+                i += 2;
+                flag = i;
+            }
+        }
+        procStr += str.substring(flag,len);
+        return procStr;
+    }
+
+    public int array667(int[] nums) {
+        if (nums.length < 2) return 0;
+        int matchCount = 0;
+        for (int i = 0; i < nums.length -1 ; i++) {
+            if (nums[i] == 6 && (nums[i+1] == 7 || nums[i+1] == 6)) matchCount++;
+        }
+        return matchCount;
+    }
+
+    public boolean noTriples(int[] nums) {
+        for
+        return
+    }
 }
